5eb37d414aa149076277bc3784a760f487c17333
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-30 01:36:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,13 @@
 function forLoop(array) {
+  newArray = array.slice(1);
   for (let i = 0; i < 25; i++) {
     if (i === 0) {
-      array.push("I am a strange loop.");
       array.push("I am 1 strange loop.");
     }
     else {
       array.push("I am " + (i + 1) + " strange loops.");
     }
   }
-  newArray = array.slice();
   console.log(newArray);
   return newArray;
 }
