a7d2e89e31ef05f008551be3c1cc0380694008bd
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-30 01:40:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
 function forLoop(array) {
-  newArray = array.slice(1);
+  newArray = array.slice();
   for (let i = 0; i < 24; i++) {
     if (i === 0) {
+      newArray.push("I am a strange loop.");
       newArray.push("I am 1 strange loop.");
     }
     else {
