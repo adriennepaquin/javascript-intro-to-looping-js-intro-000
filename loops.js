31aab3953d63b2fa456fe4f437ae97192f82fbf0
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-30 01:06:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,14 +2,15 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     //array.push("I am ${i + 1} strange loop${i === 0 ? \'\' : \'s\'}.");
     if (i === 0) {
-      console.log("I am 1 strange loop.");
-      //array.concat("I am 1 strange loop.");
+      //console.log("I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     }
     else {
-      console.log("I am " + (i + 1) + " strange loops.");
+      array.push("I am " + (i + 1) + " strange loops.");
       //array.concat("I am " + (i + 1) + " strange loops.");
     }
   }
+  console.log(array);
 }
 
 function whileLoop(n) {
