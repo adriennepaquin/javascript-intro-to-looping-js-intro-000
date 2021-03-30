1e0b3b7ba735d5efb92cf5ab544392b1013026eb
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-30 01:38:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  newArray = array.slice(1);
+  newArray = array.slice();
   for (let i = 0; i < 25; i++) {
     if (i === 0) {
       newArray.push("I am 1 strange loop.");
